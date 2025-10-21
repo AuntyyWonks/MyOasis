88b945ba46033a3a5ca7b733ce73024807ebe36a
Tankiso Montse <tmontse16@gmail.com>
2025-10-22 00:36:34 +0200

    added header section

--- client-react/src/components/layout/Hero.tsx
+++ client-react/src/components/layout/Hero.tsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import Header from './Header'
 
 function Hero() {
   return (
     <div className='hero w-full h-screen'>
-      <div className='w-full h-full bg-black/20'></div>
+      <div className='w-full h-full bg-black/20 flex flex-col items-center py-14'>
+        <Header />
+      </div>
     </div>
   )
 }
