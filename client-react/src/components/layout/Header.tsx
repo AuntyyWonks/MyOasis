79bf11466ddcd02d40f39f36c0bfe9fe6d6d8b08
Tankiso Montse <tmontse16@gmail.com>
2025-10-22 02:57:43 +0200

    added headline and subheadline to hero section & the background video

--- client-react/src/components/layout/Header.tsx
+++ client-react/src/components/layout/Header.tsx
@@ -4,7 +4,7 @@ function Header() {
   const navItems = ['Home', 'About', 'Why it matters']
 
   return (
-    <header className='w-lg h-14 rounded-2xl border border-white/35 backdrop-blur-[1.5px]'>
+    <header className='w-lg h-14 rounded-2xl bg-black/15 border border-white/35 backdrop-blur-[1.5px]'>
       <nav className='h-full'>
         <ul className='w-full h-full inline-flex text-2xl justify-evenly items-center'>
           {navItems.map((item) => (
--- client-react/src/components/layout/Hero.tsx
+++ client-react/src/components/layout/Hero.tsx
@@ -3,11 +3,31 @@ import Header from './Header'
 
 function Hero() {
   return (
-    <div className='hero w-full h-screen'>
-      <div className='w-full h-full bg-black/20 flex flex-col items-center py-14'>
-        <Header />
+    <div className="w-full h-screen relative overflow-hidden">
+      <video
+        className="absolute top-0 left-0 w-full h-full object-cover z-0"
+        autoPlay
+        muted
+        loop
+      >
+        <source src="/oasis-hero.mp4" type="video/mp4" />
+        Your browser does not support the video tag.
+      </video>
+
+      /* Overlay and content */
+      <div className="w-full h-full bg-black/30 flex flex-col items-center py-14 relative z-10">
+        <div className="relative h-4/5 z-20 flex flex-col items-center justify-between">
+          <Header />
+          <div className="w-full flex flex-col gap-12 text-center">
+            <div className='flex flex-col gap-4 text-white tracking-tighter'>
+              <h1 className="text-6xl font-bold">Oasis</h1>
+              <p className="text-lg px-12">Your AI farming assistant to help you plant, grow, and care for crops with confidence</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
+
   )
 }
 
