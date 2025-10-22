aa9eb7c9eb746b38c206d40366c1b12f167b58fc
Tankiso Montse <tmontse16@gmail.com>
2025-10-22 02:58:51 +0200

    created a prompt bar container

---
+++ client-react/src/components/PromptBar.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PromptBar() {
+  return (
+    <div className="w-full h-34 rounded-[2.5rem] border border-white/35 backdrop-blur-[1.5px] bg-black/15"></div>
+  )
+}
+
+export default PromptBar
